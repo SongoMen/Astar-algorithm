bd1bcf2f4d8537264fb501656be34e8d28c7ebb6
SongoMen <pat.kozlowski2000@onet.pl>
2019-12-02 22:06:32 +0100

    generate board

---
+++ js/board.js
@@ -0,0 +1,29 @@
+let headerHeight = document.getElementById("header").clientHeight,
+  body = document.body,
+  html = document.documentElement;
+
+let height = Math.floor(
+  (Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  ) -
+    headerHeight) /
+    26
+);
+
+let width = Math.floor(document.body.clientWidth / 25);
+
+for(let i =0;i<height;i++){
+    let row = document.createElement("div");
+    row.className = `row-${i} row`;
+    for(let b=0;b<width;b++){
+        let block = document.createElement("div");
+        row.appendChild(block);
+        block.className = `block`;
+        block.id = `block-${b}`
+    }
+    document.getElementById("area").appendChild(row)
+}   
