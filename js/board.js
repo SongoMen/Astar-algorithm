7cb60b9ea4eb18974c9dd7ff3aeed40a4079e54e
SongoMen <pat.kozlowski2000@onet.pl>
2019-12-04 18:57:01 +0100

    create walls while holding mouse button

--- js/board.js
+++ js/board.js
@@ -1,34 +1,56 @@
-let headerHeight = document.getElementById("header").clientHeight,
-  body = document.body,
-  html = document.documentElement;
+function renderArea() {
+  let headerHeight = document.getElementById("header").clientHeight,
+    body = document.body,
+    html = document.documentElement;
 
-let height = Math.floor(
-  (Math.max(
-    body.scrollHeight,
-    body.offsetHeight,
-    html.clientHeight,
-    html.scrollHeight,
-    html.offsetHeight
-  ) -
-    headerHeight) /
-    26
-);
+  let height = Math.floor(
+    (Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    ) -
+      headerHeight) /
+      26
+  );
 
-let width = Math.floor(document.body.clientWidth / 25);
+  let width = Math.floor(document.body.clientWidth / 25);
 
-for(let i =0;i<height;i++){
+  for (let i = 0; i < height; i++) {
     let row = document.createElement("div");
     row.className = `row-${i} row`;
-    for(let b=0;b<width;b++){
-        let block = document.createElement("div");
-        row.appendChild(block);
-        block.className = `block`;
-        block.id = `block-${b}`
-        block.onclick=function(){
-            this.className="block wall";
-            console.log("x")
-
+    for (let b = 0; b < width; b++) {
+      let block = document.createElement("div");
+      row.appendChild(block);
+      block.className = `block`;
+      block.id = `block-${b}`;
+      block.onclick = function() {
+        if (this.className === "block wall") {
+          this.className = "block";
+        } else {
+          this.className = "block wall";
         }
+      };
     }
-    document.getElementById("area").appendChild(row)
-}   
+    document.getElementById("area").appendChild(row);
+  }
+}
+renderArea();
+
+//build walls on hold mouse button
+
+$(".block").on("mousedown mouseup", function mouseState(e) {
+  if (e.type == "mousedown"){
+    $(document).on("mouseover", ".block", function(e) {
+      if (this.className === "block wall") {
+        this.className = "block";
+      } else if (this.className === "block") {
+        this.className = "block wall";
+      }
+    });
+  }
+  else{
+    $(document).off("mouseover")
+  }
+});
