1018488933dc1ba275ad555d16a56818a9754047
SongoMen <songomengaming@gmail.com>
2019-12-26 02:10:16 +0100

    todo detect blocks

--- js/movePoints.js
+++ js/movePoints.js
@@ -22,47 +22,46 @@ $(".starting").on("mousedown mouseup", function mouseState(e) {
   if (e.type == "mousedown") {
     console.log("x2");
     let mydiv1 = document.querySelector(".startingsvg");
-    $(".startingsvg").on("mousemove", function(e) {
+    $(".area").on("mousemove", function(e) {
       let x = e.pageX;
       let y = e.pageY;
       let elY, elX;
-        let counter = 0;
-            let stack = [];
-       let elementMouseIsOver = document.elementFromPoint(x, y);
+      let counter = 0;
+      let stack = [];
+      let elementMouseIsOver = document.elementFromPoint(x, y);
 
-
-      while (elementMouseIsOver.tagName !== "HTML") {
-      stack.push(elementMouseIsOver)
+        while (counter < 4) {
+        stack.push(elementMouseIsOver);
         elementMouseIsOver.style.pointerEvents = "none";
         elementMouseIsOver = document.elementFromPoint(x, y);
         if (elementMouseIsOver.classList.contains("row")) {
-          console.log(elementMouseIsOver.classList.value);
           elY = elementMouseIsOver.classList.value.split("-")[1].split(" ")[0];
         }
+          console.log(elementMouseIsOver.classList)
         if (elementMouseIsOver.classList.contains("block")) {
           elX = elementMouseIsOver.classList.value.split("-")[1].split(" ")[0];
         }
-          counter++
+        counter++;
       }
-      console.log( document.querySelector(
-        `.row-${elY} .block-${elX}`,
-      ));
-      document.querySelector(
-        `.row-${elY} .block-${elX}`,
-      ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="startingsvg"><polygon points="5 3 19 12 5 21 5 3"/></svg>`;
-      /* Now clean it up */
 
       var i = 0,
-        il = counter
+        il = counter;
 
       for (; i < il; i += 1) {
         stack[i].style.pointerEvents = "";
+      }
+
+        //console.log(elY, elX);
+      if (typeof elY !== "undefined" && typeof elX !== "undefined") {
+        document.querySelector(
+          `.row-${elY} .block-${elX}`,
+        ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="startingsvg"><polygon points="5 3 19 12 5 21 5 3"/></svg>`;
       }
       mydiv1.style.top = y - 15 + "px";
       mydiv1.style.left = x - 10 + "px";
     });
   } else {
     console.log("x");
-    $(".startingsvg").off("mousemove");
+    $(".area").off("mousemove");
   }
 });
