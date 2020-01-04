1703dfe3fa10b8371ae83b9961c2fcaa0fdce164
SongoMen <songomengaming@gmail.com>
2020-01-04 18:23:19 +0100

    error on finding parent element

--- js/movePoints.js
+++ js/movePoints.js
@@ -1,58 +1,66 @@
 const remove = el => el.parentElement.removeChild(el);
 
-$(".startingsvg").on("mousedown mouseup", function mouseState(e) {
-  let elY, elX;
-  console.log(e.type);
-  if (e.type == "mousedown") {
-    console.log("x2");
-    let mydiv1 = document.querySelector(".startingsvg");
-    $(".area").on("mousemove", function(e) {
-      let x = e.pageX;
-      let y = e.pageY;
-      let counter = 0;
-      let stack = [];
-      let elementMouseIsOver = document.elementFromPoint(x, y);
+function moveHandler(svgClass, blockClass) {
+  $(svgClass).on("mousedown mouseup", function mouseState(e) {
+    let elY, elX;
+    console.log(e.type);
+    if (e.type == "mousedown") {
+      console.log("x2");
+      let mydiv1 = document.querySelector(svgClass);
+      $(".area").on("mousemove", function(e) {
+        let x = e.pageX;
+        let y = e.pageY;
+        let counter = 0;
+        let stack = [];
+        let elementMouseIsOver = document.elementFromPoint(x, y);
 
-      while (counter < 1) {
-        stack.push(elementMouseIsOver);
-        elementMouseIsOver.style.pointerEvents = "none";
-        elementMouseIsOver = document.elementFromPoint(x, y);
-        if (elementMouseIsOver.classList.contains("row")) {
-          elY = elementMouseIsOver.classList.value.split("-")[1].split(" ")[0];
+        while (counter < 1) {
+          stack.push(elementMouseIsOver);
+          elementMouseIsOver.style.pointerEvents = "none";
+          elementMouseIsOver = document.elementFromPoint(x, y);
+          if (elementMouseIsOver.classList.contains("row")) {
+            elY = elementMouseIsOver.classList.value
+              .split("-")[1]
+              .split(" ")[0];
+          }
+          if (elementMouseIsOver.classList.contains("block")) {
+            elX = elementMouseIsOver.classList.value
+              .split("-")[1]
+              .split(" ")[0];
+          }
+          counter++;
         }
-        if (elementMouseIsOver.classList.contains("block")) {
-          elX = elementMouseIsOver.classList.value.split("-")[1].split(" ")[0];
+
+        var i = 0,
+          il = counter;
+
+        for (; i < il; i += 1) {
+          stack[i].style.pointerEvents = "";
         }
-        counter++;
-      }
+        if (typeof elY !== "undefined" && typeof elX !== "undefined") {
+          let targetBlock = document.querySelector(`.row-${elY} .block-${elX}`);
+          if (!targetBlock.classList.contains("ending")) {
+              console.log(svgClass)
+              console.log(document.querySelector(svgClass))
+            remove(document.querySelector(svgClass));
+            $(".block").removeClass(blockClass);
 
-      var i = 0,
-        il = counter;
+            targetBlock.classList.add(blockClass);
+            targetBlock.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=${svgClass}><polygon points="5 3 19 12 5 21 5 3"/></svg>`;
 
-      for (; i < il; i += 1) {
-        stack[i].style.pointerEvents = "";
-      }
-	          if (typeof elY !== "undefined" && typeof elX !== "undefined") {
+            //console.log(elY, elX);
 
-      let targetBlock = document.querySelector(`.row-${elY} .block-${elX}`);
-      if (!targetBlock.classList.contains("ending")) {
-        remove(document.querySelector(".startingsvg"));
-        $(".block").removeClass("starting");
-
-          targetBlock.classList.add("starting");
-          targetBlock.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="startingsvg"><polygon points="5 3 19 12 5 21 5 3"/></svg>`;
-        
-        //console.log(elY, elX);
-
-        mydiv1.style.top = y - 15 + "px";
-        mydiv1.style.left = x - 10 + "px";
-        startY = elY;
-        startX = elX;
-      }
-			  }
-    });
-  } else {
-    console.log("x");
-    $(".area").off("mousemove");
-  }
-});
+            mydiv1.style.top = y - 15 + "px";
+            mydiv1.style.left = x - 10 + "px";
+            startY = elY;
+            startX = elX;
+          }
+        }
+      });
+    } else {
+      console.log("x");
+      $(".area").off("mousemove");
+    }
+  });
+}
+moveHandler(".startingsvg","starting")
