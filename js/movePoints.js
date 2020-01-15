89769cea617a8b398e8f8c533c03e2686477cd3c
SongoMen <pat.kozlowski2000@onet.pl>
2020-01-15 21:47:08 +0100

    fixed wrong class for svgs

--- js/movePoints.js
+++ js/movePoints.js
@@ -1,12 +1,12 @@
 const remove = el => el.parentElement.removeChild(el);
 
 function moveHandler(svgClass, blockClass) {
-  $(svgClass).on("mousedown mouseup", function mouseState(e) {
+  $("." + svgClass).on("mousedown mouseup", function mouseState(e) {
     let elY, elX;
     console.log(e.type);
     if (e.type == "mousedown") {
       console.log("x2");
-      let mydiv1 = document.querySelector(svgClass);
+      let icon = document.querySelector("." + svgClass);
       $(".area").on("mousemove", function(e) {
         let x = e.pageX;
         let y = e.pageY;
@@ -40,20 +40,18 @@ function moveHandler(svgClass, blockClass) {
         if (typeof elY !== "undefined" && typeof elX !== "undefined") {
           let targetBlock = document.querySelector(`.row-${elY} .block-${elX}`);
           if (!targetBlock.classList.contains("ending")) {
-              console.log(svgClass)
-              console.log(document.querySelector(svgClass))
-            remove(document.querySelector(svgClass));
+            console.log(svgClass);
+            console.log(document.querySelector("." + svgClass));
+            remove(icon);
             $(".block").removeClass(blockClass);
 
             targetBlock.classList.add(blockClass);
-            targetBlock.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=${svgClass}><polygon points="5 3 19 12 5 21 5 3"/></svg>`;
-
+            if (blockClass === "starting") {
+              targetBlock.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=${svgClass}><polygon points="5 3 19 12 5 21 5 3"/></svg>`;
+            } else {
+              targetBlock.innerHTML = `<svg class=${svgClass} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 20.9l4.95-4.95a7 7 0 1 0-9.9 0L12 20.9zm0 2.828l-6.364-6.364a9 9 0 1 1 12.728 0L12 23.728zM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 2a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/></svg>`;
+            }
             //console.log(elY, elX);
-
-            mydiv1.style.top = y - 15 + "px";
-            mydiv1.style.left = x - 10 + "px";
-            startY = elY;
-            startX = elX;
           }
         }
       });
@@ -63,4 +61,5 @@ function moveHandler(svgClass, blockClass) {
     }
   });
 }
-moveHandler(".startingsvg","starting")
+moveHandler("startingsvg", "starting");
+moveHandler("endingsvg", "ending");
